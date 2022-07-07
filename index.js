bd48223329a38c3be35e927a2684791d8e9b2391
Archana Shinde <vyawahare.archana@gmail.com>
2022-07-07 19:34:04 -0400

    added reveal message and movie title in message

--- index.js
+++ index.js
@@ -20,22 +20,31 @@ const addMovie = (evt) => {
   inputField.value = " ";
 };
 const deleteMovie = (evt) => {
-  messaage.textContent = " Movie Deleted !";
-  console.log(messaage.textContent);
+  const title = evt.target.parentNode.textContent
+  messaage.textContent = `${title} Deleted !`;
+
+  //console.log(messaage.textContent);
+  revealMessage();
   evt.target.parentNode.remove();
 };
-
 document.querySelector("form").addEventListener("submit", addMovie);
-
 // Now that we can add and remove movies from our list, we can add to our app by allowing users to mark items as watched by clicking on a movie’s title. The CSS has already been set up to display list items differently if they have the checked class. We need to create a function that will toggle the checked class on any movie title.
 
 const crossOffMovie = (evt) => {
   evt.target.classList.toggle("checked");
+  const title = evt.target.textContent;
   if (evt.target.classList.contains("checked") === true) {
-    messaage.textContent = "  ‘Movie watched!’ !";
-    console.log(messaage.textContent);
+    messaage.textContent = `${title} watched!’ !`;
   } else {
-    messaage.textContent = "  ‘Movie added Back!!’ !";
-    console.log(messaage.textContent);
+    messaage.textContent = `${title} added back!`;
+    // console.log(messaage.textContent);
   }
+  revealMessage();
 };
+
+function revealMessage() {
+  messaage.classList.remove("hide");
+  setTimeout(() => {
+    messaage.classList.add("hide");
+  }, 1000);
+}
